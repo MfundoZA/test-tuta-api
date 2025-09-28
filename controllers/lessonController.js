2713f9d2abfdfa0366240cf33207813862e8969d
MfundoZA <jordantenza@gmali.com>
2025-09-28 15:24:38 +0200

    Removed CORS package

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -1,7 +1,6 @@
 const Database = require('better-sqlite3');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
 
 // Initialize SQLite database
 const db = new Database('./data/lessons.db');
